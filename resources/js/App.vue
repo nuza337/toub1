6fdc1f651175160595e3095e2236459e32523874
nuza337 <nuza337123@gmail.com>
2024-11-14 20:34:00 +0700

    commit 01 upload project toub1

---
+++ resources/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+   <div>
+
+   </div>
+</template>
+<script>
+export default{
+    name: 'minipos6App',
+
+    data() {
+        return{
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+<style iang="scss" scoped>
+
+</style>
